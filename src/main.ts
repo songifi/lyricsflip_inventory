c782f4261cca8e6318529e891f1d10aa16bc52c5
abdegenius <johnnieabijah2000@gmail.com>
2025-06-03 21:59:38 +0100

    Feat: Global Filters Setup

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,16 @@ import { ValidationPipe } from "@nestjs/common";
 import { AllExceptionsFilter } from "utils/exceptions.filter";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
+import { GlobalExceptionFilter } from './filters/http-exception.filter';
+import { Logger } from "winston";
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+
+  app.useGlobalFilters(new GlobalExceptionFilter(app.get(Logger)));
+
   const { httpAdapter } = app.get(HttpAdapterHost);
 
   // CORS configuration
