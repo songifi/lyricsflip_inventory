82400d755fc52d88271de03341abb8a179ec1838
nnodim <nnodimobinna@gmail.com>
2025-06-03 15:20:32 +0100

    feat: integrate Swagger for API documentation

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from "./app.module";
 import helmet from "helmet";
 import { ValidationPipe } from "@nestjs/common";
 import { AllExceptionsFilter } from "utils/exceptions.filter";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -27,6 +28,26 @@ async function bootstrap() {
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
   // Security headers
   app.use(helmet()); // Add helmet for security headers
+
+  const config = new DocumentBuilder()
+    .setTitle("Inventory Management System")
+    .setDescription("Inventory Management System API")
+    .setVersion("1.0")
+    .addBearerAuth(
+      {
+        type: "http",
+        scheme: "bearer",
+        bearerFormat: "JWT",
+        name: "JWT",
+        description: "Enter JWT token",
+        in: "header",
+      },
+      "access-token"
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api/docs", app, document);
 
   await app.listen(3000);
 }
